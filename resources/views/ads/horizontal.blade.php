d89fae3f0ff975d5072f7eacb0bc9e873d5ba169
Alvin <xenodus@gmail.com>
2020-10-28 07:34:01 +0800

    Updated adsense code

--- resources/views/ads/horizontal.blade.php
+++ resources/views/ads/horizontal.blade.php
@@ -3,11 +3,9 @@
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     <!-- ccb_horizontal -->
     <ins class="adsbygoogle"
-         style="display:block"
+         style="display:inline-block; max-width:100%; width:100%; height:100px;"
          data-ad-client="ca-pub-2393161407259792"
-         data-ad-slot="9849971769"
-         data-ad-format="auto"
-         data-full-width-responsive="true"></ins>
+         data-ad-slot="9849971769"></ins>
     <script>
          (adsbygoogle = window.adsbygoogle || []).push({});
     </script>
